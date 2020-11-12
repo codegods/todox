49594918d7d7fdf7790f197c2b96fd0489ef62b8
jalaj-k <67556827+jalaj-k@users.noreply.github.com>
2020-11-12 12:06:42 +0000

    Update deploy with master

---
+++ precache-manifest.5860ceff43b24bf43060c9d13bc556c0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1536195f388dc3fe4c4d8a7c601693",
+    "url": "/todox/index.html"
+  },
+  {
+    "revision": "b36e27c113a1efa52ede",
+    "url": "/todox/static/js/0.e384d530.chunk.js"
+  },
+  {
+    "revision": "5e1c47f8f1ce1bd4373f",
+    "url": "/todox/static/js/1.95054321.chunk.js"
+  },
+  {
+    "revision": "93669409fe710c03562d",
+    "url": "/todox/static/js/10.ddee2f2f.chunk.js"
+  },
+  {
+    "revision": "9539161ac09f606942b9",
+    "url": "/todox/static/js/11.a5b8636b.chunk.js"
+  },
+  {
+    "revision": "b8176a428807dcd1602e",
+    "url": "/todox/static/js/12.0d4a1279.chunk.js"
+  },
+  {
+    "revision": "c14f6147b11a3a504542",
+    "url": "/todox/static/js/13.987c6ace.chunk.js"
+  },
+  {
+    "revision": "2c203e42a52a887951b1",
+    "url": "/todox/static/js/14.bf2bfbf5.chunk.js"
+  },
+  {
+    "revision": "5819c96ee1d4eb4a1610",
+    "url": "/todox/static/js/15.157f6bce.chunk.js"
+  },
+  {
+    "revision": "df09e7a060e08ec4753b",
+    "url": "/todox/static/js/16.282bfa04.chunk.js"
+  },
+  {
+    "revision": "eab3967762d46e69f4ef",
+    "url": "/todox/static/js/17.aab2ae63.chunk.js"
+  },
+  {
+    "revision": "62968acf27341fb07de1",
+    "url": "/todox/static/js/2.9dab10b6.chunk.js"
+  },
+  {
+    "revision": "d47549e898c2b2cdd7fe",
+    "url": "/todox/static/js/3.13d11890.chunk.js"
+  },
+  {
+    "revision": "137d6291fe0023b3c99d",
+    "url": "/todox/static/js/4.445660fa.chunk.js"
+  },
+  {
+    "revision": "8ed48ca5413523f3df8d",
+    "url": "/todox/static/js/5.ddfd2bd1.chunk.js"
+  },
+  {
+    "revision": "e440f393090268bb5593",
+    "url": "/todox/static/js/6.85e74364.chunk.js"
+  },
+  {
+    "revision": "794fa909ef067fe06e48",
+    "url": "/todox/static/js/9.3641774c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todox/static/js/9.3641774c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a26ec17858601be462c",
+    "url": "/todox/static/js/main.7eaada11.chunk.js"
+  },
+  {
+    "revision": "8e57d2c045958336fd50",
+    "url": "/todox/static/js/runtime-main.3d8e0cbc.js"
+  },
+  {
+    "url": "/todox/bg_offline.jpg",
+    "revision": "5YM1m5HWLdHPGH4zg45oyslh"
+  }
+]);
